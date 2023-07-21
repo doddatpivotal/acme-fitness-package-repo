b4c4441bc24e9c53e88cbb087b6ceb5885112d66
Brian Kirkland <bkirkland@vmware.com>
2023-07-21 10:20:27 -0400

    1.0.3 acme-fitness release

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,6 +19,12 @@ ytt -f packages/acme-fitness/1.0.2/package-template.yaml \
     --data-value-file openapi=<(ytt -f packages/acme-fitness/1.0.2/bundle/config/values-schema.yaml --data-values-schema-inspect -o openapi-v3) \
     -v version="1.0.2" > package-repo/packages/acme-fitness.external.demo-dependencies.learn/1.0.2.yaml
 
+kbld -f packages/acme-fitness/1.0.3/bundle/config --imgpkg-lock-output packages/acme-fitness/1.0.3/bundle/.imgpkg/images.yml
+# Generate package file from from template
+ytt -f packages/acme-fitness/1.0.3/package-template.yaml \
+    --data-value-file openapi=<(ytt -f packages/acme-fitness/1.0.3/bundle/config/values-schema.yaml --data-values-schema-inspect -o openapi-v3) \
+    -v version="1.0.3" > package-repo/packages/acme-fitness.external.demo-dependencies.learn/1.0.3.yaml
+
 # Copy over metada file
 cp packages/acme-fitness/metadata.yaml package-repo/packages/acme-fitness.external.demo-dependencies.learn/
 
